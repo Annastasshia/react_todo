b990f7c9c840d25b61b7db8235e491fb3b539366
Annastasshia Ames <aames26@gmail.com>
2020-08-18 12:55:30 -0400

    Create Array of todos

--- src/App.js
+++ src/App.js
@@ -5,11 +5,32 @@ import './App.css';
 
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'laundry',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'wash the dishes',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Vacuum',
+        completed: false
+      }
+    ]
+  }
+
   render() {
+    console.log(this.state.todos)
     return (
       <div className="App">
         <h1>app</h1>
-        <Todos />
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
